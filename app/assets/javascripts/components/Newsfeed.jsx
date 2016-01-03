5540d2f91a2ac10437f28b1015e603661faa0611
spongah <rocksolid801@gmail.com>
2016-01-03 03:00:01 -0700

    more upgrades to UI

--- app/assets/javascripts/components/Newsfeed.jsx
+++ app/assets/javascripts/components/Newsfeed.jsx
@@ -4,7 +4,7 @@ var Newsfeed = React.createClass ({
 			<div>
 				{this.props.news.map( function (news_item, i) {
 					return (
-					<div className={news_item.type} key={i}>{news_item.msg}</div>
+						<div className={news_item.type} key={i}>{news_item.msg}</div>
 					);
 				})}
 			</div>
