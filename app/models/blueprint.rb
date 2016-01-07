3847c7bf5db326c46ecc06ade15bdcf21954df95
spongah <rocksolid801@gmail.com>
2016-01-06 20:32:35 -0700

    blueprint map coded into blueprint model, 8 zones

--- app/models/blueprint.rb
+++ app/models/blueprint.rb
@@ -13,10 +13,63 @@ class Blueprint < ActiveRecord::Base
 		x = options[:x].to_i
 		y = options[:y].to_i
 		z = options[:z].to_i
-		zone = 1
+		zone = 2
 
-		if z=0 && (-50..50).include?(x || y) then zone = 2 end
-		if z=0 && x <= 10 && y <= 10 then zone = 1 end
+		if z==0 then
+			#desert
+			if (x <= 5) && (y <= 5) then zone = 4 end
+			if (x >= 5) && (y <= 5) && (y <= (5 - (0.5 * x))) then zone = 4 end
+
+			#northwest forest
+			if (x <= -5) && (y >= 5) then zone = 2 end
+			if (x <= -5) && (y <= (x * -1)) && (y >= 5) then zone = 2 end
+
+			#jungle
+			if (x <= -5) && (y > (x * -1)) then zone = 3 end
+			if (x >= -5) && (y >= -5) then zone = 3 end
+			if (x >= 5) && (y >= (-5 - (0.5 * x))) then zone = 3 end
+
+			#northeast forest
+			if (x >= 50) && (y >= 30) then zone = 2 end
+			if (x >= 30) && (y >= (30 - (0.5 * x))) then zone = 2 end
+
+			#urban area
+			#if (-5..5).include?(x && y) then zone = 1 end
+			if ((x <= 5) && (x >= -5)) && ((y <= 5) && (y >= -5)) then zone = 1 end
+
+			#northwest lake
+			if (-82..-76).include?(x) && (51..57).include?(y) then zone = 7 end
+
+			#old temple area
+			if (98..114).include?(x) && (102..120).include?(y) then zone = 8 end
+
+			#old temple courtyard
+			if (100..110).include?(x) && (95..101).include?(y) then zone = 9 end
+
+			#ancient pathway
+			if (x > 5 && x < 50) && (y == 0 || y == 1) then zone = 10 end
+			if (x >= 50) && y == ((x - 50) || y == (x - 50)) then zone = 10 end
+			if (x == 100 || x == 101) && (y >= 50 && y <= 94) then zone = 10 end 
+
+			#river
+
+			#UFO crash landing
+
+			#desert oasis
+
+			#burried treasure
+
+			#desert entrance
+			if (x == -2) && (y == -3) then zone = 4 end
+			if (x == -3) && (y == -4) then zone = 4 end
+			if (x == -4) && (y == -5) then zone = 4 end
+			
+
+		end
+
+		if z < 0 then zone = 5 end 	#below ground
+		if z > 0 then zone = 6 end	#above ground
+
 
 
 		self.nameZone({ zone: zone, position: options[:position], type: options[:type] })
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,116 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Room.create(x: 0, y: 0, z: 0, name: "Salt Lake City", description: "Salt Lake City, often shortened to Salt Lake or SLC, is the capital and the most populous city in the U.S. state of Utah. With an estimated population of 191,180 in 2013,[2] the city lies at the core of the Salt Lake City metropolitan area, which has a total population of 1,153,340 (2014 estimate).", 
-	e: 3, w: 2, n: 6, s: 7, ne: 11)
-Room.create(x: -1, y: 0, z: 0, name: "West Valley", description: "West Valley City is a city in Salt Lake County and a suburb of Salt Lake City in the U.S. state of Utah. The population was 129,480 at the 2010 census,[3] making it the second-largest city in Utah. The city incorporated in 1980 from a large, quickly growing unincorporated area, which was variously known as Granger, Hunter, Chesterfield, and Redwood.", 
-	e: 1)
-Room.create(x: 1, y: 0, z: 0, name: "Sugarhouse", description: "Sugar House is home to two shopping centers that collectively feature various retailers such as Shopko, Toys \"R\" Us, bookseller Barnes & Noble, clothing retailer Old Navy, Whole Foods Market, Bed Bath & Beyond, Petco, several fast food and family restaurants, and a 10-screen Cinemark cinema.", 
-	w: 1, e: 4)
-Room.create(x: 2, y: 0, z: 0, name: "I-80 East", description: "Driving....", 
-	e: 5, w: 3)
-Room.create(x: 3, y: 0, z: 0, name: "Rock Springs, ID", description: "Rock Springs is a city in Sweetwater County, Wyoming, United States. The population was 23,036 at the 2010 census. Rock Springs is the principal city of the Rock Springs micropolitan statistical area, which has a population of 37,975.", 
-	w: 4, n: 10)
-Room.create(x: 0, y: 1, z: 0, name: "Ogden", description: "Ogden /ˈɒɡdɛn/ is a city and the county seat of Weber County,[4] Utah, United States, approximately 10 miles (16 km) east of the Great Salt Lake and 40 miles (64 km) north of Salt Lake City. The population was 82,825 according to the 2010 Census.[5] The city served as a major railway hub through much of its history,[6] and still handles a great deal of freight rail traffic which makes it a convenient location for manufacturing and commerce.", 
-	s: 1)
-Room.create(x: 0, y: -1, z: 0, name: "Orem", description: "Orem is a city in Utah County, Utah, United States, in the north-central part of the state. It is adjacent to Provo, Lindon, and Vineyard and is about 45 miles (72 km) south of Salt Lake City. Orem is one of the principal cities of the Provo-Orem, Utah Metropolitan Statistical Area, which includes all of Utah and Juab counties.", 
-	n: 1, s: 8)
-Room.create(x: 0, y: -2, z: 0, name: "Provo", description: "Provo /ˈproʊvoʊ/ is the third-largest city in the US state of Utah, located 43 miles (69 km) south of Salt Lake City along the Wasatch Front. Provo is the largest city and county seat of Utah County. It lies between the cities of Orem to the north and Springville to the south.", 
-	n: 7, s: 9)
-Room.create(x: 0, y: -3, z: 0, name: "St. George", description: "Saint George (Greek: Γεώργιος Georgios; Latin: Georgius; AD 275–281 to 23 April 303), according to legend, was a soldier in the Roman army who later became venerated as a Christian martyr. His parents were Christians of Greek background;[3] his father Gerontius was a Roman army official from Cappadocia and his mother Polychronia was a Christian from Lydda in the Roman province of Syria Palaestina (Palestine).", 
-	n: 8)
-Room.create(x: 3, y: 1, z: 0, name: "Montana", description: "Montana Listeni/mɒnˈtænə/ is a state in the Western United States. The state's name is derived from the Spanish word montaña (mountain). Montana has several nicknames, although none official,[5] including \"Big Sky Country\" and \"The Treasure State\", and slogans that include \"Land of the Shining Mountains\" and more recently \"The Last Best Place\".[6] Montana is ranked 4th in size, but 44th in population and 48th in population density of the 50 United States.", 
-	s: 5)
-Room.create(x: 1, y: 1, z: 0, name: "Park City", description: "Park City is a city in Summit County, Utah, United States. It is considered to be part of the Wasatch Back. The city is 32 miles (51 km) southeast of downtown Salt Lake City and 19.88 miles (31.99 km) from Salt Lake City's east edge of Sugar House along Interstate 80. The population was 7,558 at the 2010 census.", 
-	sw: 1)
+#1
+Room.create(x: 0, y: 0, z: 0, name: "North Plaza", description: "You are in the north end of the plaza. Everyone is busy and rushing somewhere. The Guild is north, Arena to the east, and you see a large Temple to the west!", 
+	n: 8, ne: 9, e: 4, s: 2, w: 3)
 
-Blueprint.create(map_zone: 1, data_type: "name", data_position: 1, data_value: "Urban")
-Blueprint.create(map_zone: 1, data_type: "name", data_position: 1, data_value: "City")
-Blueprint.create(map_zone: 1, data_type: "name", data_position: 1, data_value: "Industrial")
+#2
+Room.create(x: 0, y: -1, z: 0, name: "South Plaza", description: "This is the south end of the placa. There are people fire-dancing here and selling crystals. To the west is the Armor shop, south is the Magic shop, and Weapons are to the east. You see a great sand desert to the southwest.", 
+	n: 1, s: 6, sw: 14, w: 5, e: 7)
+
+#3
+Room.create(x: -1, y: 0, z: 0, name: "Temple", description: "You are in the temple. This is a holy place of solitude and refuge. There are names of people written everywhere inside, and the shapes are very detailed, beautiful, and soothing. There are two other people here meditating in silence.", 
+	e: 1)
+
+#4
+Room.create(x: 1, y: 0, z: 0, name: "Arena", description: "You are in the arena! Prepare to fight! There are 4 different doors where enemies may enter from once the gong has been rung!", 
+	w: 1, d: 13)
+
+#5
+Room.create(x: -1, y: -1, z: 0, name: "Armor Shop", description: "You are in the armor shop. The blacksmith is forging a new piece of armor on the anvil. It is hot in here!", 
+	e: 2)
+
+#6
+Room.create(x: 0, y: -2, z: 0, name: "Magic Shop", description: "This is the magic shop. It smells mysteriously like moth balls in here. You dont see anyone, but you sense there is someone in the room with you. There are scrolls and wands on the east wall, and potions on the west.", 
+	n: 2)
+
+#7
+Room.create(x: 1, y: -1, z: 0, name: "Weapons Shop", description: "You are in the weapons shop! The swordsmith greets you with open arms and offers you a beer. He continues to work as he talks to you about his favorite thing: weapons!", 
+	w: 2)
+
+#8
+Room.create(x: 0, y: 1, z: 0, name: "Guild", description: "This is the guild. There are ninjas training to the left, and archers training to the right. You feel inspired to level up as you eye the punching bag in front of you.", 
+	s: 1, n: 10, d: 12)
+
+#9
+Room.create(x: 1, y: 1, z: 0, name: "Inn", description: "This is the inn. It's cozy in here. There is a fire in the fire place and the smell of fresh cookies coming from the kitchen. \"Have a seat!\" says the bartender as you walk in.", 
+	sw: 1, u: 9)
+
+#10
+Room.create(x: 0, y: 2, z: 0, name: "Dock", description: "You are on the dock. The sea has dried up, but the dock remains. There is an old harpoon stuck in a tree here.", 
+	s: 8)
+
+#11
+Room.create(x: 1, y: 1, z: 1, name: "Private Room", description: "You are in the private room above the inn. It's very quiet in here.", 
+	d: 9)
+
+#12
+Room.create(x: 0, y: 1, z: -1, name: "Vault", description: "You enter the vault beneath the guild! This is where you can store your money and most prized inventory items, for a fee of course!", 
+	u: 8)
+
+#13
+Room.create(x: 1, y: 0, z: -1, name: "Dungeon Entrance", description: "This is the entrance to the dungeon. You tremble with the thoughts of others who have attempted to explore this god-forsaken catacomb.", 
+	u: 4)
+
+#14
+Room.create(x: -1, y: -2, z: 0, name: "Desert Entrance", description: "You are at the desert entrance. The desert seems to have receeded since this became the desert entrance because there is still probably 9 more rooms before entering the desert.. Wierd!", 
+	ne: 2)
+
+
+
+Blueprint.create(map_zone: 1, data_type: "name", data_position: 1, data_value: "Cobblestone")
+Blueprint.create(map_zone: 1, data_type: "name", data_position: 1, data_value: "Dirt")
+Blueprint.create(map_zone: 1, data_type: "name", data_position: 1, data_value: "Busy")
 Blueprint.create(map_zone: 1, data_type: "name", data_position: 2, data_value: "Street")
 Blueprint.create(map_zone: 1, data_type: "name", data_position: 2, data_value: "Parking Lot")
 Blueprint.create(map_zone: 1, data_type: "name", data_position: 2, data_value: "Sidewalk")
-Blueprint.create(map_zone: 1, data_type: "description", data_position: 1, data_value: "This city is built entirely out of a single description!")
-Blueprint.create(map_zone: 1, data_type: "description", data_position: 1, data_value: "This city seems to have randomly selected descriptions! I think they are just one position.")
-Blueprint.create(map_zone: 1, data_type: "description", data_position: 2, data_value: "Just kidding, it's got two parts.. ")
-Blueprint.create(map_zone: 1, data_type: "description", data_position: 2, data_value: "Haha ok maybe not. It seems to have two parts! Thats nice, this is very furutistic!")
-Blueprint.create(map_zone: 2, data_type: "name", data_position: 1, data_value: "Beautiful")
-Blueprint.create(map_zone: 2, data_type: "name", data_position: 1, data_value: "Wide Open")
-Blueprint.create(map_zone: 2, data_type: "name", data_position: 1, data_value: "Very Tropical")
+Blueprint.create(map_zone: 1, data_type: "description", data_position: 1, data_value: "You are walking through a bustling medievel city. ")
+Blueprint.create(map_zone: 1, data_type: "description", data_position: 1, data_value: "This is a major urban area full of creativity and innovative energy!")
+Blueprint.create(map_zone: 1, data_type: "description", data_position: 2, data_value: "There are street performers, vendors, and the smell of baked bread everywhere!")
+Blueprint.create(map_zone: 1, data_type: "description", data_position: 2, data_value: "Someone bumps into you and quicky said \"I'm sorry!\" before shuffling off...")
+
+
+Blueprint.create(map_zone: 2, data_type: "name", data_position: 1, data_value: "Mysterious")
 Blueprint.create(map_zone: 2, data_type: "name", data_position: 2, data_value: "Forest")
-Blueprint.create(map_zone: 2, data_type: "name", data_position: 2, data_value: "Grass Field")
-Blueprint.create(map_zone: 2, data_type: "name", data_position: 2, data_value: "Pasture")
-Blueprint.create(map_zone: 2, data_type: "description", data_position: 1, data_value: "The sky is blue, and the grass is green!")
-Blueprint.create(map_zone: 2, data_type: "description", data_position: 1, data_value: "Wow, what a landscape! You can see for miles!")
-Blueprint.create(map_zone: 2, data_type: "description", data_position: 2, data_value: "A bird flies past you and lands on a tree nearby. It starts to sing a song. Amazing!")
-Blueprint.create(map_zone: 2, data_type: "description", data_position: 2, data_value: "You feel like frolicking through the fields and picking flowers. It's beautiful out here!")
+Blueprint.create(map_zone: 2, data_type: "description", data_position: 1, data_value: "You are in a foggy, spooky forest. There are ferns and pine trees, and it smells of mushrooms and moss. You realize there are spiders EVERYWHERE!")
+
+Blueprint.create(map_zone: 3, data_type: "name", data_position: 1, data_value: "Dense")
+Blueprint.create(map_zone: 3, data_type: "name", data_position: 2, data_value: "Jungle")
+Blueprint.create(map_zone: 3, data_type: "description", data_position: 1, data_value: "This is a dense jungle. There are snakes, alligators, dragonflies, and most of all MUD!")
+
+Blueprint.create(map_zone: 4, data_type: "name", data_position: 1, data_value: "Sandy")
+Blueprint.create(map_zone: 4, data_type: "name", data_position: 2, data_value: "Desert")
+Blueprint.create(map_zone: 4, data_type: "description", data_position: 1, data_value: "You are thirsty! There is sand everywhere and the sun is scorching you. The wind is blowing hard from the west with no smell of water.")
+
+Blueprint.create(map_zone: 5, data_type: "name", data_position: 1, data_value: "Zone")
+Blueprint.create(map_zone: 5, data_type: "name", data_position: 2, data_value: "Five")
+Blueprint.create(map_zone: 5, data_type: "description", data_position: 1, data_value: "This is a blank zone.")
+
+Blueprint.create(map_zone: 6, data_type: "name", data_position: 1, data_value: "Zone")
+Blueprint.create(map_zone: 6, data_type: "name", data_position: 2, data_value: "Six")
+Blueprint.create(map_zone: 6, data_type: "description", data_position: 1, data_value: "Sterile zone. Nothing is here.")
+
+Blueprint.create(map_zone: 7, data_type: "name", data_position: 1, data_value: "Windy")
+Blueprint.create(map_zone: 7, data_type: "name", data_position: 2, data_value: "Lake")
+Blueprint.create(map_zone: 7, data_type: "description", data_position: 1, data_value: "You are floating out on a windy lake! The waves are lapping at your face as you tread water.")
+
+
+Blueprint.create(map_zone: 8, data_type: "name", data_position: 1, data_value: "Temple")
+Blueprint.create(map_zone: 8, data_type: "name", data_position: 2, data_value: "Ruins")
+Blueprint.create(map_zone: 8, data_type: "description", data_position: 1, data_value: "You are at some old, overgrown, temple ruins. They look to be mayan or maybe martian! Whoa..")
+
+
+Blueprint.create(map_zone: 9, data_type: "name", data_position: 1, data_value: "Overgrown")
+Blueprint.create(map_zone: 9, data_type: "name", data_position: 2, data_value: "Courtyard")
+Blueprint.create(map_zone: 9, data_type: "description", data_position: 1, data_value: "You are in the overgrown, ancient temple courtyard in front of the Temple Ruins. This place has a wierd energy. o.O")
+
+
+Blueprint.create(map_zone: 10, data_type: "name", data_position: 1, data_value: "Ancient")
+Blueprint.create(map_zone: 10, data_type: "name", data_position: 2, data_value: "Pathway")
+Blueprint.create(map_zone: 10, data_type: "description", data_position: 1, data_value: "This is an ancient pathway that is no longer used. The bushes are growing so high that its hard to make out in some spots, but its clear this used to be a busy travel route.")
+
+
+
+
+
